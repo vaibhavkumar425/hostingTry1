d2ed4887cbf18e8107c238035ace7015be84317b
Vaibhav Kumar <kumarvaibhav26072003@gmail.com>
2024-05-27 03:16:11 +0530

    third commit

--- src/app/docs/[[...slug]]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound(){
-    return (
-        <div>
-            <h2>
-                Review not found.
-            </h2>
-        </div>
-    )
-}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function reviewDetail({params}:{params:{reviewId:string,productId:string}}){
-    return <h1>Review Id is {params.reviewId} and product id is {params.productId}</h1>
-}
